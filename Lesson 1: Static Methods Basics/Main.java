6e7b034366eeca85c5bc0b8a176676a49597f1f1
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-05-24 17:53:29 +0800

    Create Main.java

---
+++ Lesson 1: Static Methods Basics/Main.java
@@ -0,0 +1,19 @@
+// Syzu 5/24/25
+// Main.java
+
+// This is the Main file
+public class Main {
+  public static void main(String[] args) {
+    
+    GreetHelper.sayHi(); // this is static
+
+    // If non static
+
+    /* Create object first
+     * GreetHelper myHelper = new Greethelper()
+     * 
+     * myHelper.sayHi();
+     */
+  }
+}
+
